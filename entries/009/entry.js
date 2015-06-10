6773db0e620dfea200f159197561b01d7d7fb032
Chad Jenkins <odestcj@gmail.com>
2015-06-10 17:34:45 -0400

    added pac sprite animation example

---
+++ entries/009/entry.js
@@ -0,0 +1,25 @@
+// note there can be no characters after the line break
+// note there can be no single quotes in the entry
+
+document.write('\
+\
+<h2>Atlanta Hawks Pac-Man</h2>\
+\
+<p class="post-info">\
+Posted by <a href="http://github.com/odestcj">odestcj</a> \
+| Filed under <a href="index.html">intermediate</a>, \
+<a href="index.html">JavaScript</a>\
+</p>\
+\
+     <p>Sprite animation of a Pac-Man character in the style of the 1980s Atlanta Hawks logo.  Use the arrow keys to move the character around.  Press any other key to reset.\
+    </p>\
+\
+<iframe\
+  style="width: 100%; height: 500px"\
+  src="http://jsfiddle.net/odestcj/6vpah8Lm/embedded/result,js/">\
+</iframe>\
+\
+');
+
+/*
+*/
